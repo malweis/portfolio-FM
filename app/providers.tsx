5cb77274b576b03f1b7554d8ce189b41772c2656
Federico Molas <flmolas@tu.com.py>
2024-07-29 10:23:12 -0400

    feat: configures pageleave capture

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,6 +8,8 @@ if (typeof window !== "undefined") {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only",
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    capture_pageleave: true // Enable pageleave capture
+
   });
 }
 
